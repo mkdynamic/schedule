3d1e0f30770036e44ce675009039516fe84d0f04
Mark Dodwell <mark@mkdynamic.co.uk>
2011-02-05 20:19:23 -0800

    try email notifier with sendmail and report errors better

--- lib/schedule/notifier/email.rb
+++ lib/schedule/notifier/email.rb
@@ -1,5 +1,6 @@
 require "schedule/notifier/base"
 require "net/smtp"
+require "tempfile"
 
 module Schedule
   module Notifier
@@ -8,14 +9,28 @@ module Schedule
 
       def notify!
         mail = []
-        mail << "From: #{@options[:from]} <#{@options[:from]}>"
+        mail << "From: #{@options[:from]} <#{@options[:from]}>" if @options[:from]
         mail << "To: #{@options[:to]} <#{@options[:to]}>"
         mail << "Subject: #{subject}"
         mail << "\n#{@message}"
         mail = mail.join("\n")
 
-        Net::SMTP.start("localhost") do |smtp|
-          smtp.send_message(mail, @options[:from], @options[:to])
+        begin
+          Net::SMTP.start("localhost") do |smtp|
+            smtp.send_message(mail, @options[:from], @options[:to])
+          end
+        rescue
+          mail_file = Tempfile.new("mail")
+          mail_file.write(mail)
+          mail_file.close
+          begin
+            result = system("sendmail #{@options[:to]} < #{File.expand_path(mail_file.path)}")
+            raise unless result
+          rescue => e
+            raise "Could not deliver email using either local SMTP or sendmail!"
+          ensure
+            mail_file.unlink rescue nil
+          end
         end
       end
 
